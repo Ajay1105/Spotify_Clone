1ec9958aa8e73b01152d7169377d4b0ab729efa1
Ajay <aky11052003@gmail.com>
2022-03-27 19:09:01 +0530

    cloning spotify

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("welcome");
+document.querySelector("h1").innerHTML = "Welcome to cloned spotify";
+let audioElement = new Audio("songs/1.mp3");
+let masterClass = document.querySelector(".play");
+let gif = document.querySelector("#gif");
+let progressBar = document.querySelector("#myProgressBar");
+
+masterClass.addEventListener("click", ()=>{
+    if(audioElement.paused || audioElement.currentTime == 0){
+    audioElement.play();
+    masterClass.classList.remove("fa-play-circle");
+    masterClass.classList.add("fa-pause-circle");
+    gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+    masterClass.classList.remove("fa-pause-circle");
+    masterClass.classList.add("fa-play-circle");
+    gif.style.opacity = 0;
+    }
+})
+
+audioElement.addEventListener("timeupdate", ()=>{
+    progress = parseInt(audioElement.currentTime/audioElement.duration * 100);
+    progressBar.value = progress;
+})
+progressBar.addEventListener("change",()=>{
+    audioElement.currentTime = (progressBar.value * audioElement.duration) /100;
+})
